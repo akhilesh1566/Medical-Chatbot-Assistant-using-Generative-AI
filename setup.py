b055a57a6b9c0c01b8e7e77242cd9cf063062b67
akhilesh0090 <158557506+akhilesh0090@users.noreply.github.com>
2025-04-30 10:09:41 +0530

    Initial commit - uploading medical chatbot project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical chat bot',
+    version= '0.0.0',
+    author= 'Akhilesh',
+    author_email= 'a1k9h9i9@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
